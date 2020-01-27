459a1fb835a1dc5532c39705b84463f3196e98db
Cameron Wong <cameron@Camerons-MacBook-Air.local>
2020-01-27 19:53:18 +1300

    Updates

---
+++ precache-manifest.e3be76a5e3569b7d56f70139fa731e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08914dfc3cac319afbe68263f96d6460",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "004bfd6f4e2270827241",
+    "url": "/monsters/static/css/main.6a7b5d1f.chunk.css"
+  },
+  {
+    "revision": "4205f318e26dcb05f19d",
+    "url": "/monsters/static/js/2.4d19465e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.4d19465e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "004bfd6f4e2270827241",
+    "url": "/monsters/static/js/main.4304f9bb.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
